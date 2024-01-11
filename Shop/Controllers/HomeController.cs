e9c2fbc031535a5f6e703b47db8607ee47972d35
MariaKukucheva <133159997+MariaKukucheva@users.noreply.github.com>
2024-01-11 10:26:12 +0200

    начало

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -1,32 +1,96 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Shop.Models;
+//using Shop.ActionFilters;
+//using Shop.Repositories;
+//using Shop.Entities;
+//using Shop.ExtentionMethods;
+//using Shop.Models;
+using Shop.ViewModels.Home;
 using System.Diagnostics;
 
 namespace Shop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+        public IActionResult Login()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Registration()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+        public IActionResult CreateVM()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+
+
+       
+
+        //[HttpGet]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult Login(LoginVM model)
+        //{
+        //    if (!this.ModelState.IsValid)
+        //        return View(model);
+
+        //    ShopDbContext context = new ShopDbContext();
+        //    User loggedUser = context.Users.Where(u => u.Username == model.Username &&
+        //                                               u.Password == model.Password)
+        //                                   .FirstOrDefault();
+        //    if (loggedUser == null)
+        //    {
+        //        this.ModelState.AddModelError("authError", "Invalid username or password!");
+        //        return View(model);
+        //    }
+
+        //    HttpContext.Session.SetObject("loggedUser", loggedUser);
+
+        //    return RedirectToAction("Index", "Home");
+        //}
+
+        //[AuthenticationFilter]
+        //public IActionResult Logout()
+        //{
+        //    HttpContext.Session.Remove("loggedUser");
+
+        //    return RedirectToAction("Login", "Home");
+        //}
     }
 }
---
+++ Shop/Views/Home/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.ViewModels.Home
+{
+    public class LoginVM
+    {
+        [DisplayName("Username: ")]
+        [Required(ErrorMessage = "Това поле е задължително!")]
+        public string Username { get; set; }
+
+        [DisplayName("Password: ")]
+        [Required(ErrorMessage = "Това поле е задължително!")]
+        public string Password { get; set; }
+    }
+}
